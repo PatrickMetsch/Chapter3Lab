2a1e63fe2069e98b19106a50d49e0192774979f2
Patrick Metsch <patrickmetsch@Patricks-MacBook-Pro.local>
2020-09-03 00:14:17 -0400

    PriceQuotation

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = 0;
+            ViewBag.Total = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountAmount = model.CalculateDiscount();
+                ViewBag.Total = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.Total = 0;
+            }
+            
+            return View(model);
+        }
+    }
+}
---
+++ Models/PriceQuotationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a subtotal.")]
+        [Range(0.01, Double.MaxValue, ErrorMessage = "Subtotal must be greater than 0")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percentage.")]
+        [Range(0, 100, ErrorMessage = "Discount percentage must be between 0 and 100")]
+        public int? DiscountPercent { get; set; }
+
+        public double? CalculateDiscount()
+        {
+            double? discount = DiscountPercent * 0.01;
+            return Subtotal * discount;
+        }
+
+        public double? CalculateTotal()
+        {
+            return Subtotal - CalculateDiscount();
+        }
+    }
+}
